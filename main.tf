cdb1aa103faff79a2d1747544cb10315597285b5
liranfar <l.farage@gmail.com>
2020-09-26 16:05:45 +0300

    changes user permissions

--- main.tf
+++ main.tf
@@ -164,16 +164,16 @@ resource "aws_instance" "web-server-instance" {
                 #!/bin/bash
                 sudo yum update -y
                 sudo yum install git -y
-                sudo amazon-linux-extras install docker
-                sudo yum install docker
+                sudo yum install docker -y
                 sudo service docker start
                 sudo usermod -a -G docker ec2-user
-                sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-                sudo chmod +x /usr/local/bin/docker-compose
-                git clone https://github.com/liranfar/Terraform-Skeleton.git
-                cd Terraform-Skeleton
+                sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
+                sudo chmod +x /bin/docker-compose
+                git clone https://github.com/liranfar/Terraform-Skeleton.git /home/ec2-user/Terraform-Skeleton
+                cd /home/ec2-user/Terraform-Skeleton
                 sudo chmod +x run_services.sh
-                ./run_services.sh
+                #su - ec2-user -c "./run_services.sh"
+                sudo ./run_services.sh
                 EOF
   tags = {
     Name = "web-server"
